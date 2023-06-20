63acb935c4e7781addb51bf51447d23b590c51ef
Khadiza083 <khadizakhatun4083@gmail.com>
2023-06-20 19:53:06 +0600

    fontawesome added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/prop-types */
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
-const Product = ({ product }) => {
-    console.log(product);
+const Product = ({ product, cartHandle }) => {
+    // console.log(product);
     const { name, price, seller, ratings, img } = product
+
+
+    
+
+
     return (
-        <div className="ms-24 w-[250px] my-24 border border-gray-200 rounded-lg relative h-[450px]">
+        <div className="ms-24 w-[250px] my-5 border border-gray-200 rounded-lg relative h-[450px]">
             <div className="p-2">
                 <img src={img} alt="" />
                 <h4 className="text-lg mt-3">{name}</h4>
@@ -12,7 +19,7 @@ const Product = ({ product }) => {
                 <p className="mt-5"><small>Manufacturer: {seller}</small></p>
                 <p><small>Ratings: {ratings}</small></p>
             </div>
-            <button className="bg-[#FFE0B3] w-full absolute bottom-0 py-3 hover:bg-orange-300">Add to Cart</button>
+            <button onClick={ () => cartHandle(product)} className="bg-[#FFE0B3] w-full absolute bottom-0 border border-[#95A0A7] rounded-b-lg py-3 hover:bg-orange-300">Add to Cart <FontAwesomeIcon icon={faCartPlus} /></button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,7 +5,7 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([])
-
+const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetch('products.json')
@@ -13,16 +13,24 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    const cartHandle = (product) => {
+        // console.log(product);
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
 
     return (
         <div className="products">
-            <div className='grid grid-cols-3'>
+            <div className='grid grid-cols-3 my-16'>
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product => <Product key={product.id} product={product} cartHandle={cartHandle}></Product>)
                 }
             </div>
-            <div className='bg-slate-200'>
-                <h2>Order summary</h2>
+            <div className='bg-[#FF99004D] '>
+                <h2 className='text-xl text-center mt-7'>Order summary</h2>
+                <div className='py-12 px-3'>
+                    <p>Selected Items: {cart.length} </p>
+                </div>
             </div>
         </div>
     );
